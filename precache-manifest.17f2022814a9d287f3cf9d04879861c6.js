798a3cb93ffea2a830637838cba99a62651a4f59
web-stan <50701017+web-stan@users.noreply.github.com>
2020-05-04 17:05:24 +0300

    Updates

---
+++ precache-manifest.17f2022814a9d287f3cf9d04879861c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88815e3450c377bd5bc65a689f84d359",
+    "url": "/react-quiz1/index.html"
+  },
+  {
+    "revision": "a5ccae85d044f5cbfd66",
+    "url": "/react-quiz1/static/css/main.c5f9dca1.chunk.css"
+  },
+  {
+    "revision": "15bc514958f4985fa4d7",
+    "url": "/react-quiz1/static/js/2.9d886c44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz1/static/js/2.9d886c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ccae85d044f5cbfd66",
+    "url": "/react-quiz1/static/js/main.8a448fc2.chunk.js"
+  },
+  {
+    "revision": "a6933a686b1010d2b9cb",
+    "url": "/react-quiz1/static/js/runtime-main.a18ae8e6.js"
+  }
+]);
